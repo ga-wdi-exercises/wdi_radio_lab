51027ce3ba28d86ea0b91dd9564edac04483451c
Andrew Whitley <awhitley1233@gmail.com>
2017-08-18 11:30:25 -0400

    add player custom directive to songsIndex

--- wdi_radio/js/directives/player.directive.js
+++ wdi_radio/js/directives/player.directive.js
@@ -5,11 +5,25 @@
       songPlayer
     ])
 
+
   function songPlayer () {
     return {
       templateUrl: 'js/directives/player.html',
       scope: {
         song: '='
+      },
+      link: function ($scope, $element) {
+        $scope.closePlayer = function () {
+          $scope.$parent.vm.selectedSong = null
+          $element.find('audio')[0].pause()
+        }
+        $element.find('audio').bind('ended', function () {
+          console.log('end function')
+          let randomIndex = Math.floor(Math.random() * $scope.$parent.vm.songs.length)
+          let randomSong = $scope.$parent.vm.songs[randomIndex]
+          $scope.$parent.vm.selectSong(randomSong)
+          $scope.$apply()
+        })
       }
     }
   }
